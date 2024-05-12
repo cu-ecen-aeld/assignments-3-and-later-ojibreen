dac5636bce4a0923fdf34e85f3951585e821f298
omar <omar.jibreen@gmail.com>
2024-05-12 20:56:19 +0000

    Assignment 8: Updated aesdsocket start-stop script.

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -4,11 +4,11 @@
 case "$1" in 
 	start)
 		echo  -n "Starting aesdsocket daemon "
-		start-stop-daemon -S -n aesdsocket -a aesdsocket -- -d && echo " -  Sucessfully started" || echo " -  Failed to start"
+		start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d && echo " -  Sucessfully started" || echo " -  Failed to start"
 		;;
 	stop)
 		echo -n "Stopping aesdsocket daemon "
-		start-stop-daemon -K -n aesdsocket -a aesdsocket && echo " -  Sucessfully stopped" || echo " -  Failed to stop"
+		start-stop-daemon -K -n aesdsocket -a /usr/bin/aesdsocket && echo " -  Sucessfully stopped" || echo " -  Failed to stop"
 		;;
 	*)
 		echo "Usage $0 {start|stop}"
